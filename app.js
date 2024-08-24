d8991c171a095458b8d50778778ab38f65cd5df6
ADITYA KUMAR <152451674+dhanrajad@users.noreply.github.com>
2024-08-25 00:33:39 +0530

    Add files via upload
    js files

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-// add new feature - button
+// let msg = "     Hi baby           ";
+
+// let password = prompt("set your password");
+// console.log(password.trim());
+
+// let name = "Apna Colllege";
+
+// let msg = "error";
+// console.log(msg.toUpperCase());
+
+// let msg = "              habibi        ";
+// let nwMsg = msg.trim().toUpperCase();
+
+// let msg = "dhanrajcompany";
+// console.log(msg.slice(5));
+
+// let msg = "ILoveCoding";
+// console.log(msg.toUpperCase());
+
+// let name = "ApnaCollege";   
+
+// let cars = ["audi", "bmw", "xuv", "maruti" ];
+
+// let months = ["january", "july", "march", "august" ];
+
+// let game = [ ['X', null, 'O'],[null, 'X', null], ['O', null, 'X']]; 
+
+
+
+// 3. Write a JavaScript program to check whether a string is blank or not.
+// let str = prompt("pleae enter a string");
+
+// if(str.length == 0)  {
+//     console.log("string is empty");
+// } else {
+//     console.log("string is not empty");
+// }
+
+// Qs4. Write a JavaScript program to test whether the character at the given (character)
+// index is lower case.
+//  let str = "ApNaColLEgEe";
+//  let idx = 3;
+
+//  if(str[idx] == str[idx].toLowerCase()) {
+//      console.log("character is lowercase");
+//  } else {
+//      console.log("character is not lowercase") ;
+//  }
+
+
+// 5. Write a JavaScript program to strip leading and trailing spaces from a String.
+// let str = prompt("pllease enter a steing");
+// console.log(`original string = ${str}`);
+// console.log(`string withSout spaces = ${str.trim()}`);
+
+// Qs6. Write a JavaScript program to check if an element exists in an array or not.
+
+let arr = ["hello", 'a', 23, 25, 98, -43];
+let item = 64;
+if(arr.indexOf(item) != -1)  {
+    console.log("element exists in array");
+} else {
+    console.log("element does not exist in array");
+}
