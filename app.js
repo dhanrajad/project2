6fb46fbef9ab60514215868b06b0df32c2997b52
ADITYA KUMAR <152451674+dhanrajad@users.noreply.github.com>
2024-08-28 23:18:44 +0530

    Add files via upload
    day18

--- app.js
+++ app.js
@@ -1,134 +1,134 @@
-// console.log("1");
-// console.log("2");
-// console.log("3");
-// console.log("4");
-// console.log("5");
-// console.log("6");
+// let delhi = {
+//     latitude: "28.7041 N" 
+//     longitude: "77.1825 E"
+// };
+
+// const student = {
+//     name: "Aditya",
+//     age: 23,
+//     marks: 89.5,
+//     city: "Delhi"
+// };
+
+// const item = {
+//     price: 100.99,
+//     discount: 40,
+//     color: ["red", "pink"]
+// };
+// const post = {
+//     username: "@dhanrajadi",
+//     content: "This is my #NewPost",
+//     likes: 180,
+//     tags: ["@dhanrajcompany","@hadup"]
+// };
+
+// let student = {
+//     name: "aditya",
+//     marks: 87.52
+// };
+
+// const obj = {
+//     1: "a",
+//     2: "b",
+//     true: "c",
+//     null: "d",
+//     undefined: "e"
+// };
+
+// const student = {
+//     name : "aditya",
+//     age : "24",
+//     marks: "78",
+//     city: "Delhi"
+// };
+
+// const classInfo = {
+//     aditya : {
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     neha : {
+//         gade: "A",
+//         city: "Pune"
+//     },
+//     karan : {
+//         grade: "O",
+//         city: "Mumbai"
+//     }
+// };
+
+// const classInfo = [
+//     {
+//         name: "aditya",
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     {
+//         name: "neha",
+//         gade: "A",
+//         city: "Pune"
+//     },
+//     {
+//         name: "Karan",
+//         grade: "O",
+//         city: "Mumbai"
+//     }
+// ]; 
+// Gessing games?
 
 
-// for (let i=10; i>=5; i--){
-//     console.log(i) ;
-// }
+// const max = prompt("enter the max number");
 
-// for (let i=2; i<=6; i++){
-//     console.log(i);
-// }
+// const random = Math.floor(Math.random() * max) + 1;
 
-// console.log("odd no");
-// for (let i=1; i<=16; i=i+2){
-//     console.log(i);
-// }
+// let guess = prompt("guess the number");
 
-// console.log("backwords");
+// while(true) {
+//     if(guess == "quit") {
+//         console.log("user quit");
+//         break;
+//     }
 
-// for (let i=15; i>=1; i=i-2){
-//     console.log(i);
-// }
-
-// console.log("even number");
-
-// for (let i=2; i<=10; i=i+2){
-//     console.log(i);
-// }
-
-// console.log("backword");
-
-// for (let i=10; i>=2; i=i-2){
-//     console.log(i);
-// }
-
-// console.log("multiplication");
-
-// for (let i=5; i<=50; i=i+5){
-//     console.log(i);
-// }
-
-// let n = prompt("write your number");
-// n = parseInt(n);
-
-// for (let i=n; i<=n*10; i=i+n){
-//     console.log(i);
-// } 
-
-// for (let i=1; i<=3; i++) {
-//     console.log(`outer loop ${i}`);
-//     for(let j=1; j<=3; j++){
-//         console.log(j);
+//     if(guess == random) {
+//         console.log("you are right! congrats!! random number was", random);
+//         break;
+//     // } else  {
+//     //     guess = prompt("your gess was wrong. please try again");
+//     // }
+//     } else if (guess < random){
+//         guess = prompt("hint : your guess was too small. please try again");
+//     } else {
+//         guess = prompt("hint : your guess was too large. please try again");
 //     }
 // }
 
- 
-// let i=1;
+// Qs1. Create a program that generates a random number representing a dice roll.
+// [The number should be between 1 and 6]
 
-// while(i<=6){
-//     console.log(i);
-//     i++;
-// }
-  
-// let i=6;
+// let dice = Math.floor( Math.random() * 6) + 1;
+// console.log(dice);
 
-// while(i>=1){
-//     console.log(i);
-//     i--;
-// }
+// Qs2. Create an object representing a car that stores the following properties for the
+// car: name, model, color.
+// Print the car’s name.
 
-
-//  const favBook = "maths";
-
-//  let guess = prompt("guess my favorite book");
-
-//  while((guess != favBook && guess != "quit")){
-//     guess = prompt("wrong guess. please try again");
-//  }
-
-//  if(guess == favBook){
-//     console.log("congrats!!");
-//  } else {
-//     console.log("you quit the game");
-//  }
-
-// const favMovie = "gollmall";
-
-//  let guess = prompt("guess my favorite movie");
-
-//  while(guess != favMovie ){
-//    if(guess == "quit"){
-//       console.log("you quit");
-//       break;  ushing break condition
-//    }
-//     guess = prompt("wrong guess. please try again");
-//  }
-
-//  if(guess == favMovie){
-//     console.log("congrats..........!!");
-//  } 
-
-// let fruits = ["mango", "apple", "banana", "litchi", "orange"];
-// fruits.push("pineapple");
-
-// for (let i=0; i<fruits.length; i++ ) {
-//    console.log(i, fruits[i]);
-// }
-// for (let i=fruits.length-1; i>=0; i-- ) {
-//    console.log(i, fruits[i]);
-// }
-
-// let heroes = [["ironman", "spiderman", "thor"], ["superman", "wonder woman", "flash"]];
-
-// for (let i=0; i<heroes.length; i++ ) {
-//    console.log(`List #${i}`);
-//    for (let j=0; j<heroes[i].length; j++ ) {
-//       console.log(heroes[i][j]);
-//    }
-// }
-// let fruits = ["mango", "apple", "banana", "orange", "litchi"];
-
-// for(fruits of fruits) {
-//    console.log(fruits);
-// }
+// const car = {
+//     name: "maruti Suzuki",
+//     model: "Maruti Suziki Dzire",
+//     color: "white"
+// };
+// console.log(car.name);
 
 
+// Qs3. Create an object Person with their name, age and city.
+// Edit their city’s original value to change it to “New York”.
+// Add a new property country and set it to the United States.
 
-
-
-
+const Person = {
+    name: "Jane Doe",
+    age: 21,
+    city: "New Jersey"
+};
+Person.city = "New York";
+Person.country = "United States";
+console.log(Person);
